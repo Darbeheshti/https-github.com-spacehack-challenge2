eddf2797424fe431b55ed1e34ad5abbde80d3f1d
Debsankha manik <dmanik@nld.ds.mpg.de>
2019-05-25 14:11:08 +0200

    local change test_transform

--- src/test_transform.py
+++ src/test_transform.py
@@ -64,162 +64,162 @@ def simple_angle_converter(pointpx, top_right, top_left, bottom_left, bottom_rig
 
 
 class TestTransformLukas(unittest.TestCase):
-	def test_unrotated_picture(self):
-		width = 10
-		height = 60
-		
-		lon_min = 20
-		lon_max = 30
-		lat_min = 10
-		lat_max = 70
-		
-	
-		top_left = np.array([lon_min, lat_max])
-		top_right = np.array([lon_max, lat_max])
-		bottom_left = np.array([lon_min, lat_min])
-		bottom_right = np.array([lon_max, lat_min])
-		
-		# check that top left is sane
-		res = transform.transform(np.array([0, 0]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, top_left))
-	
-		# check that top right is sane
-		res = transform.transform(np.array([width,0]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, top_right))
+    def test_unrotated_picture(self):
+        width = 10
+        height = 60
+        
+        lon_min = 20
+        lon_max = 30
+        lat_min = 10
+        lat_max = 70
+        
+    
+        top_left = np.array([lon_min, lat_max])
+        top_right = np.array([lon_max, lat_max])
+        bottom_left = np.array([lon_min, lat_min])
+        bottom_right = np.array([lon_max, lat_min])
+        
+        # check that top left is sane
+        res = transform.transform(np.array([0, 0]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, top_left))
+    
+        # check that top right is sane
+        res = transform.transform(np.array([width,0]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, top_right))
 
-		# check that bottom left is sane
-		res = transform.transform(np.array([0,height]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, bottom_left))
-	
-		# check that bottom right is sane
-		res = transform.transform(np.array([width,height]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, bottom_right))
+        # check that bottom left is sane
+        res = transform.transform(np.array([0,height]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, bottom_left))
+    
+        # check that bottom right is sane
+        res = transform.transform(np.array([width,height]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, bottom_right))
 
 
 
 
 class TestBasicTransform(unittest.TestCase):
-	def test_unrotated_picture(self):
-		width = 10
-		height = 60
-		
-		lon_min = 20
-		lon_max = 30
-		lat_min = 10
-		lat_max = 70
-		
-	
-		top_left = np.array([lon_min, lat_max])
-		top_right = np.array([lon_max, lat_max])
-		bottom_left = np.array([lon_min, lat_min])
-		bottom_right = np.array([lon_max, lat_min])
-		
-		# check that top left is sane
-		res = simple_angle_converter(np.array([0, 0]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, top_left))
-	
-		# check that top right is sane
-		res = simple_angle_converter(np.array([width,0]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, top_right))
+    def test_unrotated_picture(self):
+        width = 10
+        height = 60
+        
+        lon_min = 20
+        lon_max = 30
+        lat_min = 10
+        lat_max = 70
+        
+    
+        top_left = np.array([lon_min, lat_max])
+        top_right = np.array([lon_max, lat_max])
+        bottom_left = np.array([lon_min, lat_min])
+        bottom_right = np.array([lon_max, lat_min])
+        
+        # check that top left is sane
+        res = simple_angle_converter(np.array([0, 0]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, top_left))
+    
+        # check that top right is sane
+        res = simple_angle_converter(np.array([width,0]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, top_right))
 
-		# check that bottom left is sane
-		res = simple_angle_converter(np.array([0,height]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, bottom_left))
-	
-		# check that bottom right is sane
-		res = simple_angle_converter(np.array([width,height]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, bottom_right))
+        # check that bottom left is sane
+        res = simple_angle_converter(np.array([0,height]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, bottom_left))
+    
+        # check that bottom right is sane
+        res = simple_angle_converter(np.array([width,height]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, bottom_right))
 
-	def test_fourtyFive_deg(self):
-		width = 100
-		height = 100
+    def test_fourtyFive_deg(self):
+        width = 100
+        height = 100
 
-		top_left = np.array([50,50])
-		bottom_left = np.array([40,40])
-		bottom_right = np.array([50,30])
-		top_right = np.array([60,40])
+        top_left = np.array([50,50])
+        bottom_left = np.array([40,40])
+        bottom_right = np.array([50,30])
+        top_right = np.array([60,40])
 
-		res = simple_angle_converter((50,50),top_right,top_left, bottom_left, bottom_right, (width,height))
-		self.assertTrue(np.allclose(res, (40,40)))
+        res = simple_angle_converter((50,50),top_right,top_left, bottom_left, bottom_right, (width,height))
+        self.assertTrue(np.allclose(res, (40,40)))
 
 
 class TestTransformVectorTransform(unittest.TestCase):
-	def test_unrotated_picture(self):
-		width = 10
-		height = 60
-		
-		lon_min = 20
-		lon_max = 30
-		lat_min = 10
-		lat_max = 70
-		
-	
-		top_left = np.array([lon_min, lat_max])
-		top_right = np.array([lon_max, lat_max])
-		bottom_left = np.array([lon_min, lat_min])
-		bottom_right = np.array([lon_max, lat_min])
-		
-		# check that top left is sane
-		res = rc.vector_converter(np.array([0, 0]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, top_left))
-	
-		# check that top right is sane
-		res = rc.vector_converter(np.array([width,0]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, top_right))
+    def test_unrotated_picture(self):
+        width = 10
+        height = 60
+        
+        lon_min = 20
+        lon_max = 30
+        lat_min = 10
+        lat_max = 70
+        
+    
+        top_left = np.array([lon_min, lat_max])
+        top_right = np.array([lon_max, lat_max])
+        bottom_left = np.array([lon_min, lat_min])
+        bottom_right = np.array([lon_max, lat_min])
+        
+        # check that top left is sane
+        res = rc.vector_converter(np.array([0, 0]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, top_left))
+    
+        # check that top right is sane
+        res = rc.vector_converter(np.array([width,0]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, top_right))
 
-		# check that bottom left is sane
-		res = rc.vector_converter(np.array([0,height]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, bottom_left))
-	
-		# check that bottom right is sane
-		res = rc.vector_converter(np.array([width,height]), top_right, top_left,
-			bottom_left, bottom_right, np.array([width, height]))
-		self.assertTrue(np.allclose(res, bottom_right))
+        # check that bottom left is sane
+        res = rc.vector_converter(np.array([0,height]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, bottom_left))
+    
+        # check that bottom right is sane
+        res = rc.vector_converter(np.array([width,height]), top_right, top_left,
+            bottom_left, bottom_right, np.array([width, height]))
+        self.assertTrue(np.allclose(res, bottom_right))
 
-	def test_fourtyFive_deg(self):
-		width = 100
-		height = 100
+    def test_fourtyFive_deg(self):
+        width = 100
+        height = 100
 
-		top_left = np.array([50,50])
-		bottom_left = np.array([40,40])
-		bottom_right = np.array([50,30])
-		top_right = np.array([60,40])
+        top_left = np.array([50,50])
+        bottom_left = np.array([40,40])
+        bottom_right = np.array([50,30])
+        top_right = np.array([60,40])
 
-		res = rc.vector_converter((50,50),top_right,top_left, bottom_left, bottom_right, (width,height))
-		self.assertTrue(np.allclose(res, (50,40)))
+        res = rc.vector_converter((50,50),top_right,top_left, bottom_left, bottom_right, (width,height))
+        self.assertTrue(np.allclose(res, (50,40)))
 
     def test_10_deg(self):
-		width = 100
-		height = 100
+        width = 100
+        height = 100
 
-		top_left = np.array([50,50])
-		bottom_left = np.array([50,40])
-		bottom_right = np.array([60,40])
-		top_right = np.array([60,50])
+        top_left = np.array([50,50])
+        bottom_left = np.array([50,40])
+        bottom_right = np.array([60,40])
+        top_right = np.array([60,50])
 
-		res = rc.vector_converter((50, 50), top_right, top_left, bottom_left, bottom_right, (width, height))
-		self.assertTrue(np.allclose(res, (46.350258, 53.86699865)))
+        res = rc.vector_converter((50, 50), top_right, top_left, bottom_left, bottom_right, (width, height))
+        self.assertTrue(np.allclose(res, (46.350258, 53.86699865)))
 
 
-#	def test_north_poll_edge_case(self):
-#		self.fail()
+#    def test_north_poll_edge_case(self):
+#        self.fail()
 #
-#	def test_south_poll_edge_case(self):
-#		self.fail()
+#    def test_south_poll_edge_case(self):
+#        self.fail()
 #
-#	def test_zero_crossing_edge_case(self):
-#		self.fail()
+#    def test_zero_crossing_edge_case(self):
+#        self.fail()
 #
-#	def test_360_crossing_edge_case(self):
-#		self.fail()
+#    def test_360_crossing_edge_case(self):
+#        self.fail()
