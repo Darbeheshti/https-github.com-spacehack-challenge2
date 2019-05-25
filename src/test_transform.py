e17a341081743eae9db4590dc1edee7bd4536482
Debsankha manik <dmanik@nld.ds.mpg.de>
2019-05-25 11:55:22 +0200

    addtest test for Lukas tranform

--- src/test_transform.py
+++ src/test_transform.py
@@ -62,6 +62,44 @@ def simple_angle_converter(pointpx, top_right, top_left, bottom_left, bottom_rig
     return actual_lon, actual_lat
 
 
+class TestTransformLukas(unittest.TestCase):
+	def test_unrotated_picture(self):
+		width = 10
+		height = 60
+		
+		lon_min = 20
+		lon_max = 30
+		lat_min = 10
+		lat_max = 70
+		
+	
+		top_left = np.array([lon_min, lat_max])
+		top_right = np.array([lon_max, lat_max])
+		bottom_left = np.array([lon_min, lat_min])
+		bottom_right = np.array([lon_max, lat_min])
+		
+		# check that top left is sane
+		res = transform.transform(np.array([0, 0]), top_right, top_left,
+			bottom_left, bottom_right, np.array([width, height]))
+		self.assertTrue(np.allclose(res, top_left))
+	
+		# check that top right is sane
+		res = transform.transform(np.array([width,0]), top_right, top_left,
+			bottom_left, bottom_right, np.array([width, height]))
+		self.assertTrue(np.allclose(res, top_right))
+
+		# check that bottom left is sane
+		res = transform.transform(np.array([0,height]), top_right, top_left,
+			bottom_left, bottom_right, np.array([width, height]))
+		self.assertTrue(np.allclose(res, bottom_left))
+	
+		# check that bottom right is sane
+		res = transform.transform(np.array([width,height]), top_right, top_left,
+			bottom_left, bottom_right, np.array([width, height]))
+		self.assertTrue(np.allclose(res, bottom_right))
+
+
+
 
 class TestTransform(unittest.TestCase):
 	def test_unrotated_picture(self):
@@ -98,7 +136,6 @@ class TestTransform(unittest.TestCase):
 		res = simple_angle_converter(np.array([width,height]), top_right, top_left,
 			bottom_left, bottom_right, np.array([width, height]))
 		self.assertTrue(np.allclose(res, bottom_right))
-
 
 
 class TestTransformVectorTransform(unittest.TestCase):
